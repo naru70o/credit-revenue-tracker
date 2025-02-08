58ce16b3c4659e7f21d86858a7a5e12ae46857a5
kadar naruto <khadarpaashe123@gmail.com>
2025-02-08 09:18:24 +0300

    removed the code cousing the error

--- src/app/api/credits/[id]/route.ts
+++ src/app/api/credits/[id]/route.ts
@@ -2,32 +2,32 @@ import { connectiondb, Credit } from "@/lib/database/models";
 import { revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const id = params.id;
-//     console.log("i'm checking if this function runs id");
-//     await connectiondb();
-//     const credit = await Credit.findById(id);
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    console.log("i'm checking if this function runs id");
+    await connectiondb();
+    const credit = await Credit.findById(id);
 
-//     if (!credit) {
-//       return NextResponse.json(
-//         { message: "Credit not found" },
-//         { status: 404 }
-//       );
-//     }
+    if (!credit) {
+      return NextResponse.json(
+        { message: "Credit not found" },
+        { status: 404 }
+      );
+    }
 
-//     return NextResponse.json(credit);
-//   } catch (error) {
-//     console.error(error);
-//     return NextResponse.json(
-//       { message: "Internal Server Error" },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json(credit);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
 
 // delete
 export async function DELETE(
@@ -76,21 +76,21 @@ export async function PATCH(
 }
 
 // update whole document
-// export async function PUT(
-//   request: NextRequest,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const id = params.id;
-//     const credit = await request.json();
-//     await connectiondb();
-//     const updatedCredit = await Credit.findByIdAndUpdate(id, credit, {
-//       new: true,
-//     });
-//     return NextResponse.json(updatedCredit);
-//   } catch (error) {
-//     console.log(error);
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = params.id;
+    const credit = await request.json();
+    await connectiondb();
+    const updatedCredit = await Credit.findByIdAndUpdate(id, credit, {
+      new: true,
+    });
+    return NextResponse.json(updatedCredit);
+  } catch (error) {
+    console.log(error);
 
-//     return NextResponse.json({ status: 500, message: "Internal Server Error" });
-//   }
-// }
+    return NextResponse.json({ status: 500, message: "Internal Server Error" });
+  }
+}
